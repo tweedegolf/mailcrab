63bc518b79080b3d798fb2b8078aeb25b8802a61
Marlon Baeten <marlon@tweedegolf.com>
2022-10-25 14:02:30 +0200

    Speed up build

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-cargo install cross --git https://github.com/cross-rs/cross
-
 cd backend
 
 cargo build --release
 cross build --target aarch64-unknown-linux-gnu --release
-
-cargo install trunk
-rustup target add wasm32-unknown-unknown
 
 cp target/aarch64-unknown-linux-gnu/release/mailcrab-backend target/arm64
 cp target/release/mailcrab-backend target/amd64
@@ -19,5 +14,4 @@ trunk build
 
 cd ..
 
-docker buildx create --use
 docker buildx build --push --platform=linux/amd64,linux/arm64 . -t marlonb/mailcrab:latest

69c9b4f8f8b46d515f42dc1bb2b6d6bf8792a729
Marlon Baeten <marlon@tweedegolf.com>
2023-06-08 12:34:39 +0200

    Return message id after a message is parsed

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -180,7 +180,7 @@ impl TryFrom<mail_parser::Message<'_>> for MailMessage {
             _ => {
                 event!(
                     Level::WARN,
-                    "Could not parse 'From' address header, setting placeholder 'from' address."
+                    "Could not parse 'From' address header, setting placeholder address."
                 );
 
                 Address {
@@ -199,7 +199,7 @@ impl TryFrom<mail_parser::Message<'_>> for MailMessage {
             _ => {
                 event!(
                     Level::WARN,
-                    "Could not parse 'To' address header, setting placeholder 'to' address."
+                    "Could not parse 'To' address header, setting placeholder address."
                 );
 
                 vec![Address {
